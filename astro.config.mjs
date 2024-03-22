26f2a0c486fca75be5b46f673c6ada9a1f0d9de2
Sanss <santi-2000-12@hotmail.com>
2024-03-21 20:28:26 -0500

    Pushing to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), compress(), react()]
+});
