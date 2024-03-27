81fbb7e14a90861d9c53abbe68a047f2b280099c
Sanss <santi-2000-12@hotmail.com>
2024-03-26 20:54:21 -0500

    fixing db

--- db/config.ts
+++ db/config.ts
@@ -5,7 +5,6 @@ import { defineDb, column, defineTable } from 'astro:db';
 export const Users = defineTable({
   columns:{
     id: column.number({primaryKey:true}),
-    email: column.text(),
     password: column.text(),
   }
 })
